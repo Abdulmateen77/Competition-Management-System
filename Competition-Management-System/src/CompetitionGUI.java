97992bcd7898a6e701278541c35e89e883e71fa0
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2024-01-24 20:50:40 +0000

    Update CompetitionGUI.java

--- Competition-Management-System/src/CompetitionGUI.java
+++ Competition-Management-System/src/CompetitionGUI.java
@@ -6,6 +6,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+
 // Class for managing the competition GUI
 public class CompetitionGUI extends Component {
 
