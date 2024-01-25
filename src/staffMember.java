023deb74beed05eced5c1d3004249b392b0e9322
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2024-01-25 22:31:59 +0000

    Update staffMember.java

--- src/staffMember.java
+++ src/staffMember.java
@@ -9,28 +9,33 @@ public class staffMember {
     public boolean validateAccess(){
         return true;
     }
-
+    // method to get the competitor details
     public String getCompetitorDetails(){
         return "hello";
     }
+    // method to get result from 
     public int[] getResult(){
         return competitor.getScore();
     }
 
+    // method to get summary in the report format
     public String getSummaryreport(){
         return "Hi";
     }
+    // method to enter a score for a competitor
     public int enterScore(){
     }
 
+    // register a competitor
     public registerCompetitor(){
 
     }
-
+    // method to remove a competitor for the competition
     public void removeCompetitor(){
 
     }
 
+    // method to update the competitor details
     public amendCompetitor(){
 
     }
