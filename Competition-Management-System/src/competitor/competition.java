0108ba30d7aa19105d600d16e309c4a61d0e78e1
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2024-02-01 22:23:53 +0000

    Update competition.java

--- Competition-Management-System/src/competitor/competition.java
+++ Competition-Management-System/src/competitor/competition.java
@@ -1,15 +1,21 @@
 package competitor;
 
+// The competition class represents a generic competition with various attributes.
 public class competition {
+    
+    // Static attributes to store information about the competition.
     public static int competitionID;
     public static String competitionName;
     public static String Category;
     public static int ageLimit;
     public static String Level;
 
+   
     public static int getCompetitionID(){
         return competitionID;
     }
+
+    // Constructor to initialize the competition object with provided details.
     public competition(int competitionID, String competitionName, String Category, int ageLimit, String Level){
         competition.competitionID = competitionID;
         competition.competitionName = competitionName;
@@ -18,7 +24,8 @@ public class competition {
         competition.Level = Level;
     }
 
+    // Method to get a formatted string containing competition details.
     public static String getCompetitionDetails(){
-        return competitionName + "with and ID " + competitionID + " in" + Category + " with an age limit of " + ageLimit + " for " + Level;
+        return competitionName + " with an ID " + competitionID + " in " + Category + " with an age limit of " + ageLimit + " for " + Level;
     }
 }
