dea97a7039292b854717eec942138bfdc7cf566f
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2024-02-24 23:57:00 +0000

    Update CompetitorList.java

--- Competition-Management-System/src/competitor/CompetitorList.java
+++ Competition-Management-System/src/competitor/CompetitorList.java
@@ -9,7 +9,7 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.Arrays;
 
-//Competitor list class detailss
+// Competitor list class detailss
 public class CompetitorList {
     private static List<AMScompetitor> competitorList;
 
