ece656c62b6b2a0c2ab05733bb5afd011f816ae2
Abdul Mateen Syed <69627857+Abdulmateen77@users.noreply.github.com>
2024-06-16 00:44:13 +0100

    Update AMScompetitor.java

--- Competition-Management-System/src/competitor/AMScompetitor.java
+++ Competition-Management-System/src/competitor/AMScompetitor.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 //Competitor Class
 public class AMScompetitor {
     private static int competitorCount = 0;
-
     
     //Instance variables for competitor details
     private int competitorNumber;
